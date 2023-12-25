a41d1de73b5c29cd3f258dc5bd21de67d019ddfc
hikoyara <hikoyara7@icloud.com>
2023-12-25 14:12:02 +0900

    ヘッダー修正

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,12 @@
 import { Appbar } from "react-native-paper";
 
 const Header = (props) => {
-    const { children } = props;
+    const { children, back } = props;
     return (
-        <Appbar.Header>
-            <Appbar.BackAction onPress={() => {}} />
+        <Appbar.Header style={{ backgroundColor: "#F3EDF6" }}>
+            {back && <Appbar.BackAction onPress={() => {}} />}
+            <Appbar.Action icon="account-circle" onPress={() => {}} />
             <Appbar.Content title={children} />
-            <Appbar.Action icon="github" onPress={() => {}} />
         </Appbar.Header>
     );
 };
