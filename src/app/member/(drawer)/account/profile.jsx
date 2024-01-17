d5721f670bbb5181efea7d82b7bfa14642aef0e5
hikoyara <hikoyara7@icloud.com>
2024-01-17 15:50:55 +0900

    profile画面スタイリング

--- src/app/member/(drawer)/account/profile.jsx
+++ src/app/member/(drawer)/account/profile.jsx
@@ -1,15 +1,34 @@
-import { Text, StyleSheet, View } from "react-native";
-import { DrawerToggleButton } from "@react-navigation/drawer";
-import { Drawer } from "expo-router/drawer";
+import { StyleSheet, View, ScrollView } from "react-native";
+/* components */
 import Header from "../../../../components/Header";
+/* paper */
+import { Avatar, Button, Text } from "react-native-paper";
 
 export default function Profile() {
     return (
         <View style={styles.container}>
-            <Header back>account</Header>
-            <View style={styles.body}>
-                <Text style={{ fontSize: 24 }}>Index page of Settings Drawer</Text>
-            </View>
+            <Header back>My page</Header>
+            <ScrollView style={styles.body}>
+                <View style={styles.profile}>
+                    <View style={styles.profileHead}>
+                        <Avatar.Icon size={80} icon="account" />
+                        <Button icon="pencil" mode="contained-tonal" onPress={() => console.log("Pressed")}>
+                            編集する
+                        </Button>
+                    </View>
+                    <View style={styles.profileBody}>
+                        <Text variant="headlineSmall" style={styles.profileName}>
+                            テスト組合員
+                        </Text>
+                        <Text variant="bodyMedium">これはダミーテキストです。これはダミーテキストです。これはダミーテキストです。これはダミーテキストです。これはダミーテキストです。</Text>
+                        <View style={styles.profileCategoryList}>
+                            <View style={[styles.profileCategoryItem]}>
+                                <Text variant="labelMedium">飲食店</Text>
+                            </View>
+                        </View>
+                    </View>
+                </View>
+            </ScrollView>
         </View>
     );
 }
@@ -19,7 +38,32 @@ const styles = StyleSheet.create({
         flex: 1,
     },
     body: {
-        paddingTop: 100,
+        flex: 1,
+        paddingTop: 20,
         paddingHorizontal: 20,
+        backgroundColor: "#fff",
+    },
+    profileHead: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        marginBottom: 20,
+    },
+    profileName: {
+        marginBottom: 20,
+    },
+    profileCategoryList: {
+        flexDirection: "row",
+        alignItems: "center",
+        flexWrap: "wrap",
+        marginTop: 10,
+    },
+    profileCategoryItem: {
+        paddingVertical: 8,
+        paddingHorizontal: 10,
+        borderRadius: 5,
+        backgroundColor: "#E6DEF6",
+        marginTop: 10,
+        marginRight: 10,
     },
 });
