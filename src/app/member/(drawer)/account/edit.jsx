97e1e13ec8c1add2516913dabf44bc1693d2c4be
hikoyara <hikoyara7@icloud.com>
2024-01-18 13:51:41 +0900

    プロフィール画面 スタイリング

--- src/app/member/(drawer)/account/edit.jsx
+++ src/app/member/(drawer)/account/edit.jsx
@@ -2,7 +2,7 @@ import { StyleSheet, View, ScrollView } from "react-native";
 /* context */
 import { useAuthContext } from "../../../../context/UserContext";
 /* paper */
-import { Avatar, Button, Text } from "react-native-paper";
+import { Avatar, Button, Text, IconButton, TextInput } from "react-native-paper";
 /* router */
 import { router } from "expo-router";
 
@@ -18,12 +18,40 @@ export default function Edit() {
                 <Text variant="titleMedium" style={{ fontWeight: "600", color: "#fff" }}>
                     編集
                 </Text>
-                <Button style={styles.cancel} textColor="#fff" labelStyle={{ fontWeight: "600" }} onPress={() => router.back()}>
+                <Button style={styles.decide} textColor="#fff" labelStyle={{ fontWeight: "600" }} onPress={() => router.back()}>
                     決定
                 </Button>
             </View>
-            <ScrollView style={styles.body}>
-                <Avatar.Icon size={80} icon="account" />
+            <ScrollView style={styles.body} contentContainerStyle={{ paddingBottom: 120 }}>
+                <View style={styles.iconWrapper}>
+                    <View style={styles.iconInner}>
+                        <Avatar.Icon size={80} icon="account" />
+                        <IconButton
+                            icon="sync"
+                            mode="contained"
+                            size={14}
+                            onPress={() => {
+                                console.log("aaa");
+                            }}
+                            style={styles.iconButton}
+                            iconColor="#fff"
+                            containerColor="rgba(0,0,0,0.2)"
+                        />
+                    </View>
+                    <Text variant="bodySmall" style={styles.id}>
+                        組合員ID：
+                    </Text>
+                </View>
+                <View style={styles.form}>
+                    <TextInput mode="flat" label="組合員名" selectionColor="#000" style={styles.formInput} />
+                    <TextInput mode="flat" label="自己紹介" selectionColor="#000" style={styles.formInput} multiline />
+                    <TextInput mode="flat" label="住所" selectionColor="#000" style={styles.formInput} multiline />
+                    <TextInput mode="flat" label="電話番号" selectionColor="#000" style={styles.formInput} />
+                    <TextInput mode="flat" label="営業日" selectionColor="#000" style={styles.formInput} multiline />
+                    <TextInput mode="flat" label="営業時間" selectionColor="#000" style={styles.formInput} multiline />
+                    <TextInput mode="flat" label="定休日" selectionColor="#000" style={styles.formInput} multiline />
+                    <TextInput mode="flat" label="決済方法" selectionColor="#000" style={styles.formInput} multiline />
+                </View>
             </ScrollView>
         </View>
     );
@@ -36,17 +64,44 @@ const styles = StyleSheet.create({
     header: {
         backgroundColor: "#1976d2",
         flexDirection: "row",
-        justifyContent: "space-between",
+        justifyContent: "center",
         alignItems: "center",
         paddingVertical: 10,
     },
+    cancel: {
+        position: "absolute",
+        bottom: 3,
+        left: 5,
+    },
+    decide: {
+        position: "absolute",
+        bottom: 3,
+        right: 5,
+    },
     body: {
         flex: 1,
-        paddingTop: 20,
+        paddingTop: 40,
         paddingHorizontal: 20,
+        paddingBottom: 40,
         backgroundColor: "#fff",
     },
     iconWrapper: {
-        backgroundColor: "#000",
+        alignItems: "center",
+    },
+    iconButton: {
+        position: "absolute",
+        bottom: -6,
+        right: -6,
+    },
+    id: {
+        marginTop: 20,
+        color: "#707070",
+    },
+    form: {
+        marginTop: 40,
+    },
+    formInput: {
+        marginBottom: 20,
+        backgroundColor: "rgba(255, 255, 255, 0)",
     },
 });
